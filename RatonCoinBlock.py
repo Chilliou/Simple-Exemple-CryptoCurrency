978c20fc6375fa44bd180da257c77529605e605f
Chilliou <Chilliou07@gmail.com>
2023-02-07 00:01:02 +0100

    Renomage

--- RatonCoinBlock.py
+++ RatonCoinBlock.py
@@ -1,10 +1,10 @@
 import hashlib
 
-class GeekCoinBlock:
+class RatonCoinBlock:
 
-	def __init__(self, previous_block_hash, transaction_list):
-		self.previous_block_hash = previous_block_hash
-		self.transaction_list = transaction_list
+	def __init__(self, ancien_block_hash, liste_transaction):
+		self.ancien_block_hashs = ancien_block_hash
+		self.transaction_list = liste_transaction
 
-		self.block_data = f"{' - '.join(transaction_list)} - {previous_block_hash}"
-		self.block_hash = hashlib.sha256(self.block_data.encode()).hexdigest()
+		self.donnee_block = f"{' - '.join(liste_transaction)} - {ancien_block_hash}"
+		self.block_hash = hashlib.sha256(self.donnee_block.encode()).hexdigest()
